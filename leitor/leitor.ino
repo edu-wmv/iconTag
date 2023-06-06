0f4790ede28ac0e659f5ce0fdc82aae711052327
edu-wmv <montzdudu@outlook.com>
2023-06-06 17:21:05 -0300

    read-uid

---
+++ leitor/leitor.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN   9
+#define SS_PIN    10
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);		                         // inicia o comunicação do código com o serial
+  SPI.begin();                                   // inicia o spi
+  rfid.PCD_Init();                               // inicia o leitor de tag
+
+	Serial.println("Aproxime a tag do leitor");
+}
+
+void loop() {
+  if (rfid.PICC_IsNewCardPresent()) {             // tag disponível
+    if (rfid.PICC_ReadCardSerial()) {             // aqui a tag foi lida já
+
+
+      // esse código cria a variavél para armazenar as infos do tipo da tag
+      MFRC522::PICC_Type picc_Type = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(picc_Type));
+
+      // retorna UID da tag em HEX
+      Serial.print("UID: ");
+      for ( int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      // parar a leitura
+      rfid.PICC_HaltA();
+      rfid.PCD_StopCrypto1();
+    }
+  }
+
+}
