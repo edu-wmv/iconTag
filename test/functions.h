453a31086837c320d5ef0b402068ba808d33cb45
edu-wmv <montzdudu@outlook.com>
2023-07-05 15:45:42 -0300

    led_test

--- test/functions.h
+++ test/functions.h
@@ -1,11 +1,13 @@
 #include <MFRC522.h>
 #include <SPI.h>
-
+#include <LiquidCrystal.h>
 // RFID MODULE PINS
 #define RST_PIN   9 // RFID reset pin
 #define SS_PIN    8 // RFID pin
 MFRC522 rfid(SS_PIN, RST_PIN); // rfid initializator
 
+const int rs = 31, en = 33, d4 = 43, d5 = 45, d6 = 47, d7 = 49;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void tagReader() {
   // tag disponível
   if (rfid.PICC_IsNewCardPresent()) {
@@ -15,6 +17,8 @@ void tagReader() {
     if (rfid.PICC_ReadCardSerial()) {
 
       Serial.println("OK");
+
+      lcd.print("BEM VINDO AO ICON!");
 
       // parar a leitura
       rfid.PICC_HaltA();
--- test/test.ino
+++ test/test.ino
@@ -4,6 +4,9 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();
   rfid.PCD_Init();
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.print("INIT");
 
   Serial.println("Aproxime a tag do leitor: ");
 }
