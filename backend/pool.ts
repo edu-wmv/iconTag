052a49f09afa2fa6852efccef3da4ba14c5b1c5d
edu-wmv <montzdudu@outlook.com>
2023-08-20 04:13:36 -0300

    'backend-preview'

--- backend/pool.ts
+++ backend/pool.ts
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const pool = mysql.createConnection({
-    host: '192.168.0.13',
-    user: 'tagend',
-    password: 'tagendpass',
-    database: 'icon_icontag'
+    host: '192.168.1.32',
+    user: 'db_icontag',
+    password: 'icontag',
+    database: 'db_icontag'
 })
 
 module.exports = { pool }
--- backend/query.ts
+++ backend/query.ts
@@ -5,7 +5,6 @@ Tabela: iconicos (id, name, uid, pontos, hours)
 */
 
 import { Request, Response } from 'express'
-import { Query, QueryOptions } from 'mysql'
 const { pool } = require('./pool')
 import { v4 as uuidv4 } from 'uuid'
 
@@ -15,8 +14,8 @@ const insertData = (req: Request, res: Response) => {
     const uid = req.headers['uid']
 
     pool.query(
-        `INSERT INTO iconicos (id, name, uid, pontos, hours)
-         VALUES (NULL, '${name}', ${uid}, NULL, NULL)`,
+        `INSERT INTO iconicos (name, uid)
+         VALUES ('${name}', ${uid})`,
          (error: any, results: any) => {
             if (error) throw error
             res.status(200).json(`Iconico ${name} adicionado com o ID ${results.insertId}`)
@@ -46,8 +45,8 @@ const setPoint = (req: Request, res: Response) => {
 
     pool.query(
         `SELECT * FROM iconicos 
-         WHERE uid = '${uuid}'`,
-        (error: any, results: any) => {
+         WHERE uid = ${uuid}`,
+        (error: Error, results: any) => {
             if (error) throw error
             if (results.length > 0 && typeof results !== undefined) {
                 // USUARIO EXISTE
@@ -59,17 +58,17 @@ const setPoint = (req: Request, res: Response) => {
                      WHERE userId = ${userId}
                      ORDER BY data DESC
                      LIMIT 1`,
-                    (error: any, results: any) => {
+                    (error: Error, results: any) => {
                         if (error) throw error
                         
                         // BOOLEANO PARA VERIFICAR SE O USUARIO ESTA ENTRANDO OU SAINDO
-                        const isEntrada = results.length > 0 && typeof results !== undefined ? results[0].entrada : false;
+                        const last_point: boolean = results.length > 0 && typeof results !== undefined ? results[0].entrada : false
 
                         // INSERE NOVO PONTO
                         pool.query(
-                            `INSERT INTO pontos (uid, userId, uuid, name, data, entrada)
-                             VALUES ('${uuidv4()}', ${userId}, '${uuid}', '${userName}', '${data_time}', ${!isEntrada})`,
-                            (error: any, results: any) => {
+                            `INSERT INTO pontos (id_ponto, userId, uuid, name, data, entrada)
+                             VALUES ('${uuidv4()}', ${userId}, ${uuid}, '${userName}', '${data_time}', ${!last_point})`,
+                            (error: Error) => {
                                 if (error) throw error
 
                                 // ATUALIZA PONTOS DO USUARIO
@@ -78,36 +77,75 @@ const setPoint = (req: Request, res: Response) => {
                                      WHERE userId = ${userId}
                                      ORDER BY data DESC
                                      LIMIT 1`,
-                                    (error: any, results: any) => {
+                                    (error: Error, results: any) => {
                                         if (error) throw error
 
                                         // ADICIONA UID DO PONTO NA LISTA DE PONTOS DO USUARIO
-                                        const ponto_uid = results[0].uid
+                                        const ponto_uid = results[0].id_ponto
                                         pool.query(
                                             `UPDATE iconicos
                                              SET pontos = CONCAT_WS(IFNULL(',', ''), pontos, '${ponto_uid}')
                                              WHERE id = ${userId}`,
-                                            (error: any, results: any) => {
-                                                if (error) throw error
-
-                                                // ATUALIZA HORAS DO USUARIO
-                                                // SAIDA
-                                                
-                                            }
+                                            (error: Error) => { if (error) throw error }
                                         )
                                     }
                                 )
                             }
                         )
+
+                        // ATUALIZA HORAS DO USUARIO
+                        if (last_point == false) {
+                            res.status(200).json(`Bem vindo ${userName}!`)
+                        } else {
+                            try {
+                                pool.query(
+                                    `SELECT * FROM pontos
+                                     WHERE userId = ${userId}
+                                     ORDER BY data DESC
+                                     LIMIT 2`,
+                                     (error: Error, results: any) => {
+                                        if (error) throw error
+                                    
+                                        const entrada = new Date(results[1].data)
+                                        const saida = new Date(results[0].data)
+                                        const diff = (saida.getTime() - entrada.getTime()) / 1000
+                                        const time = `${toHoursAndMinutes(diff).h}:${toHoursAndMinutes(diff).m}:${toHoursAndMinutes(diff).s}`
+                                    
+                                        pool.query(
+                                            `UPDATE iconicos
+                                             SET hours = ADDTIME(hours, '${time}')
+                                             WHERE id = ${userId}`,
+                                            (error: Error) => { if (error) throw error }
+                                        )
+
+                                        res.status(200).json(`Até logo ${userName}!`)
+                                    }
+                                )
+                            } catch (error) {
+                                console.log(error)
+                                res.status(500).json(`Erro interno ao atualizar horas do usuario`)
+                            }
+                        }
                         
                     }
                 )
+
+            // USUARIO NAO EXISTE
             } else {
-                // USUARIO NAO EXISTE
                 res.status(200).json(`Iconico não encontrado`)
             }
         }
     )
+}
+
+function toHoursAndMinutes(totalSeconds: number) {
+    const totalMinutes = Math.floor(totalSeconds / 60);
+  
+    const seconds = totalSeconds % 60;
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+  
+    return { h: hours, m: minutes, s: seconds };
 }
 
 module.exports = { 
